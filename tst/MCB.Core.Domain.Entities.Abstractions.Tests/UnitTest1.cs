5a02288c38da145f7dbf603c753383b7b2d9f394
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-05 22:37:38 -0300

    feat: add test project

---
+++ tst/MCB.Core.Domain.Entities.Abstractions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MCB.Core.Domain.Entities.Abstractions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
