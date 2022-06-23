138d618ea25599a7446fa4a669e811d3aaf1fe3f
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-06-23 02:15:53 -0300

    refact: change o nullable

--- src/MCB.Core.Domain.Entities.Abstractions/Specifications/Interfaces/IDomainEntitySpecifications.cs
+++ src/MCB.Core.Domain.Entities.Abstractions/Specifications/Interfaces/IDomainEntitySpecifications.cs
@@ -8,8 +8,8 @@ public interface IDomainEntitySpecifications
     bool CreationInfoShouldRequired(DateTimeOffset createdAt, string createdBy);
     bool CreationInfoShouldValid(DateTimeOffset createdAt, string createdBy);
 
-    bool UpdateInfoShouldRequired(DateTimeOffset lastUpdateDate, string lastUpdatedBy);
-    bool UpdateInfoShouldValid(DateTimeOffset createdAt, DateTimeOffset lastUpdatedAt, string lastUpdatedBy);
+    bool UpdateInfoShouldRequired(DateTimeOffset? lastUpdateAt, string lastUpdatedBy);
+    bool UpdateInfoShouldValid(DateTimeOffset createdAt, DateTimeOffset? lastUpdatedAt, string lastUpdatedBy);
 
     bool RegistryVersionShouldRequired(DateTimeOffset registryVersion);
     bool RegistryVersionShouldValid(DateTimeOffset registryVersion);
