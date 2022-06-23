9d13b4e95c59072394e3fbed0cde517aa8124db6
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-06-23 01:15:34 -0300

    feat: add validators interfaces

---
+++ src/MCB.Core.Domain.Entities.Abstractions/Validators/Interfaces/IDomainEntityShouldValidAfterRegisterModificationValidator.cs
@@ -0,0 +1,9 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions;
+
+namespace MCB.Core.Domain.Entities.Abstractions.Validators.Interfaces;
+
+public interface IDomainEntityShouldValidAfterRegisterModificationValidator<TDomainEntity>
+    : IValidator<TDomainEntity>
+    where TDomainEntity : IDomainEntity
+{
+}
---
+++ src/MCB.Core.Domain.Entities.Abstractions/Validators/Interfaces/IDomainEntityShouldValidAfterRegisterNewValidator.cs
@@ -0,0 +1,10 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions;
+
+namespace MCB.Core.Domain.Entities.Abstractions.Validators.Interfaces;
+
+public interface IDomainEntityShouldValidAfterRegisterNewValidator<TDomainEntity>
+    : IValidator<TDomainEntity>
+    where TDomainEntity : IDomainEntity
+{
+
+}
