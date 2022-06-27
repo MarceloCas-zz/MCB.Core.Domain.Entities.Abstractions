3d20b5ecece89461272265e95045dc2c996c1041
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-06-26 21:49:38 -0300

    refact: remove validators after operation

--- src/MCB.Core.Domain.Entities.Abstractions/Validators/Interfaces/IDomainEntityShouldValidAfterRegisterModificationValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions;
-
-namespace MCB.Core.Domain.Entities.Abstractions.Validators.Interfaces;
-
-public interface IDomainEntityShouldValidAfterRegisterModificationValidator<TDomainEntity>
-    : IValidator<TDomainEntity>
-    where TDomainEntity : IDomainEntity
-{
-}
